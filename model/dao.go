9b6b905a6e74541721d66d48650c9c1e545626c6
qingmeng123 <2379224042@qq.com>
2022-11-23 04:47:39 +0800

    server update err

--- model/dao.go
+++ model/dao.go
@@ -42,7 +42,7 @@ func InitDb() {
 	}
 
 	// 迁移数据表，在没有数据表结构变更时候，建议注释不执行
-	_ = Db.AutoMigrate(&User{}, &Article{}, &Category{}, Profile{}, Comment{})
+	//_ = Db.AutoMigrate(&User{}, &Article{}, &Category{}, Profile{}, Comment{})
 
 	sqlDB, _ := Db.DB()
 	// SetMaxIdleCons 设置连接池中的最大闲置连接数。
--- web/admin/src/views/Admin.vue
+++ web/admin/src/views/Admin.vue
@@ -20,6 +20,14 @@
 import Nav from '../components/admin/Nav'
 import Footer from '../components/admin/Footer'
 import Header from '../components/admin/Header'
+
+// function setRem() {
+//   // 当前页面宽度相对于 750 宽的缩放比例，可根据自己需要修改
+//   const scale = document.documentElement.clientWidth / 750;
+//   document.documentElement.style.fontSize = scale + 'px';
+// }
+// setRem();
+// window.onresize = setRem;
 export default {
   components: { Nav, Footer, Header },
 }
